cc6eb95a77273f4d862781365087475851830127
TerSom <140949512+TerSom@users.noreply.github.com>
2024-03-28 14:52:12 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Introduction from './components/introduction';
+import Profile from './components/profile';
+
+function App() {
+  return (
+    <div>
+      <Profile/>
+      <Introduction name="Umay somay"/>
+      <Introduction name="Niayam"/>
+    </div>
+  );
+}
+
+export default App;
